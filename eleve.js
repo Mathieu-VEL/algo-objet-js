59e0e681b4068dccb60bf828ba47ce0ff364a3f7
VELVEROX <laith.shaltan@gmail.com>
2024-12-09 16:31:47 +0100

    2

--- eleve.js
+++ eleve.js
@@ -8,4 +8,18 @@ class Eleve{
     bonjour(){
         alert("bonjour, je suis "+this.nom.firstname+this.nom.name+" et j'ai "+this.age+" ans");
     }
+        average(){
+        return this.notes.algorithme
+            
+        }
 }
+
+function average(tab) {
+                
+                let sum = 0;
+                for (let i = 0; i < tab.length; i++) {
+                    sum = sum + tab[i];
+                }
+                sum = sum / tab.length;
+            }
+            average(groupeEleves);
