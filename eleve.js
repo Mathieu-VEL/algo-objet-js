31bea25b07a5243a06cba60c978fbef02ae0b8d4
VELVEROX <laith.shaltan@gmail.com>
2024-12-16 11:15:15 +0100

    algo-objet-calculemoyenne

--- eleve.js
+++ eleve.js
@@ -5,21 +5,22 @@ class Eleve{
         this.classe = c;
         this.notes = {math:[], algorithme:[], javascript:[]};
     }
+
+    
     bonjour(){
-        alert("bonjour, je suis "+this.nom.firstname+this.nom.name+" et j'ai "+this.age+" ans");
+        alert("bonjour, je suis "+this.prenom+" "+this.nom+" en classe de "+this.classe+"");
     }
-        average(){
-        return this.notes.algorithme
-            
-        }
+        // average(){
+        // return this.notes.algorithme
+        // }
 }
 
-function average(tab) {
+// function average(tab) {
                 
-                let sum = 0;
-                for (let i = 0; i < tab.length; i++) {
-                    sum = sum + tab[i];
-                }
-                sum = sum / tab.length;
-            }
-            average(groupeEleves);
+//                 let sum = 0;
+//                 for (let i = 0; i < tab.length; i++) {
+//                     sum = sum + tab[i];
+//                 }
+//                 sum = sum / tab.length;
+//             }
+//             average(groupeEleves);
